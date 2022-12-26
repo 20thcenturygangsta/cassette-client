8f73504998261da97ab5e55dbe55c10f77f4b067
Kimkyungmin123 <rud3302@naver.com>
2022-12-26 18:53:31 +0900

    chore: add types

---
+++ src/types.ts
@@ -0,0 +1,2 @@
+export type color = 'cassetteorange' | 'tangerine' | 'pink' | 'deeppink' | 'purple' | 'lightgreen' | 'olive' | 'emerald' | 'ceruleanblue' | 'brown';
+export type buttonType = 'main' | 'kakao' | 'guest' | 'clear';
