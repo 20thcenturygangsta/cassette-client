d697876b0ccca050225354ff0c9c1436299e6b29
Kimkyungmin123 <rud3302@naver.com>
2023-01-26 18:20:41 +0900

    feat: addtTape info & color pick feature

--- src/pages/guest/[id]/decorate-tape-guest.tsx
+++ src/pages/guest/[id]/decorate-tape-guest.tsx
@@ -1,31 +1,30 @@
 import Button from 'components/button';
 import ColorPlate from 'components/colorPlate';
 import Tape from 'components/tape';
-import { useColorStore } from 'store';
+import Link from 'next/link';
+import { useGuestColorStore, useGuestInfoStore } from 'store';
 import { DecoContainer, DecoZone } from 'styles/decorate-tape';
-import { Color } from 'types';
-
-export interface decorateTapeProps {
-  color: Color;
-}
 
 const DecorateTapeGuest = () => {
-  const { tapeColor } = useColorStore();
+  const { userNickname, tapename } = useGuestInfoStore();
+
+  const guestColor = useGuestColorStore().tapeColor;
 
   return (
-    <DecoContainer color={tapeColor}>
+    <DecoContainer color={guestColor}>
       <DecoZone css={{ gap: '24px' }}>
-        <h3>게스트&apos;s Tape</h3>
-        <Tape title="2023 한정판 테이프" date="21.01.01" sec="144" />
+        <h3>{userNickname}&apos;s Tape</h3>
+        <Tape title={tapename} date="21.01.01" sec="144" isOwner={false} />
         <DecoZone css={{ gap: '136px' }}>
           <div>
             <p>
               테이프 <span>안쪽 색상</span>를 골라주세요!
             </p>
-            <ColorPlate />
+            <ColorPlate isOwner={false} />
           </div>
-
-          <Button variant="main">꾸미기 완료</Button>
+          <Link href="create-tape-completed">
+            <Button variant="main">꾸미기 완료</Button>
+          </Link>
         </DecoZone>
       </DecoZone>
     </DecoContainer>
--- src/pages/guest/[id]/guest-entry.tsx
+++ src/pages/guest/[id]/guest-entry.tsx
@@ -1,27 +1,69 @@
+'use client';
+
 import Button from 'components/button';
 import Tape from 'components/tape';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useGuestResponsStore } from 'store';
 import { Container, Zone } from 'styles';
+import { Color } from 'types';
+import subInstance from 'utils/api/sub';
 
 const GuestEntry = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [ownerName, setOwnerName] = useState<string>('');
+  const [ownerTapeTitle, setOwnerTapeTitle] = useState<string>('');
+  const [ownerTapeColor, setOwnerTapeColor] =
+    useState<Color>('cassette_orange');
+  const { setResponsUser } = useGuestResponsStore();
+  const [date, setDate] = useState<string>('');
+
+  const GUEST_CREATE_URL = `/guest/${id}/create-tape-guest`;
+
+  useEffect(() => {
+    if (id) {
+      subInstance.getOwnerTape(id as string).then((data) => {
+        setOwnerName(data?.result?.name),
+          setOwnerTapeTitle(data?.result?.title),
+          setOwnerTapeColor(data?.result?.colorCode),
+          setDate(data.timestamp.slice(2, 10).replace('-', '.')),
+          setResponsUser(id as string);
+      });
+    }
+  }, [id, ownerName, ownerTapeTitle, setResponsUser]);
+
   return (
-    <Container>
-      <Zone css={{ gap: '32px' }}>
-        <div>
-          <h1>벨라&apos;s Tape</h1>
-          <h3>나의 새로운 도전을 응원해줘!</h3>
-        </div>
-        <Tape title="2023 한정판 테이프" date="21.01.01" sec="144" />
-        <p>
-          평소 전하지 못했던 마음을 <br /> 목소리로 담아보세요 ♡
-        </p>
-      </Zone>
-      <Zone css={{ paddingTop: ' 176px' }}>
-        <Link href="/decorate-tape-guest" css={{ maxWidth: '327px' }}>
-          <Button variant="guest">목소리 남겨주기</Button>
-        </Link>
-      </Zone>
-    </Container>
+    <>
+      {id ? (
+        <Container>
+          <Zone css={{ gap: '32px' }}>
+            <div>
+              <h1>{ownerName}&apos;s Tape</h1>
+              <h3>나의 새로운 도전을 응원해줘!</h3>
+            </div>
+            <Tape
+              title={ownerTapeTitle}
+              date={date}
+              sec="144"
+              color={ownerTapeColor}
+            />
+            <p>
+              평소 전하지 못했던 마음을 <br /> 목소리로 담아보세요 ♡
+            </p>
+          </Zone>
+          <Zone css={{ paddingTop: '176px' }}>
+            <Link href={GUEST_CREATE_URL} css={{ maxWidth: '327px' }}>
+              <Button variant="guest" color={ownerTapeColor}>
+                목소리 남겨주기
+              </Button>
+            </Link>
+          </Zone>
+        </Container>
+      ) : null}
+    </>
   );
 };
 
