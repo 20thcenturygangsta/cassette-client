ed47efb2fdc4962f3318f82363ff86dcb15df49b
Kimkyungmin123 <rud3302@naver.com>
2023-01-26 19:05:23 +0900

    feat: create user URL copy feature

--- src/pages/create-tape-completed.tsx
+++ src/pages/create-tape-completed.tsx
@@ -1,8 +1,9 @@
 import Button from 'components/button';
 import Tape from 'components/tape';
 import Title from 'components/title';
+import useCopy from 'hooks/useCopy';
 import { useEffect } from 'react';
-import { useResponsUserStore, useUserStore } from 'store';
+import { useColorStore, useResponsUserStore, useUserStore } from 'store';
 import theme from 'styles/theme';
 import subInstance from 'utils/api/sub';
 
@@ -11,15 +12,22 @@ import { Box } from '../styles/create-tape';
 const CreateTapeCompleted = () => {
   const { setResponsUser, userURL } = useResponsUserStore();
   const { userNickname, tapename, setUserData } = useUserStore();
+  const { setTapeColor } = useColorStore();
+  const [isCopied, onCopy] = useCopy();
 
-  const GUEST_URL = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/guest/${userURL}/guest-entry`;
+  const handleCopyClipBoard = (text: string) => {
+    onCopy(text);
+  };
+
+  const GUEST_URL = `${process.env.NEXT_PUBLIC_CLIENT_URL}/guest/${userURL}/guest-entry`;
 
   useEffect(() => {
     subInstance.getUserTape().then((data) => {
       setUserData(data?.result[0]['name'], data?.result[0]['title']);
       setResponsUser(data?.result[0]['tapeLink']);
+      setTapeColor(data?.result[0]['colorCode']);
     });
-  }, [setResponsUser, setUserData]);
+  }, [setResponsUser, setUserData, setTapeColor]);
 
   return (
     <div>
@@ -30,9 +38,13 @@ const CreateTapeCompleted = () => {
         <Tape title={tapename} date="21.01.01" sec="144" />
       </Box>
 
-      <Button variant="main" onClick={() => alert(`링크 복사 : ${GUEST_URL}`)}>
+      <Button
+        variant="main"
+        onClick={() => handleCopyClipBoard(`${GUEST_URL}`)}
+      >
         친구들에게 목소리 남겨달라고 하기
       </Button>
+      {isCopied ? <span>복사완료</span> : null}
     </div>
   );
 };
