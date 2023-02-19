6ccc23c5940174c9e45e5cf554812c06aa13795b
Kimkyungmin123 <rud3302@naver.com>
2023-02-20 01:01:15 +0900

    chore: modify make-track page

--- src/styles/make-track.ts
+++ src/styles/make-track.ts
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import { ButtonProps } from 'components/button';
+import { ButtonLayout } from 'components/button/style';
 
 import theme from './theme';
 
@@ -6,21 +8,31 @@ export const MakeTapeContainer = styled.div`
   margin: 113px 24px 0 24px;
 `;
 
-export const WarningZone = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 12px;
+export const WarningZone = styled.div<{ firstEntry: boolean }>`
+  margin: 104px 0 12px 0;
 
-  p {
-    font-family: 'Pretendard-Regular';
-    font-size: ${theme.fontSize.sm};
-    color: ${theme.colors.gray_500};
-    font-weight: 500;
-    line-height: 20px;
-    letter-spacing: 0em;
-    text-align: left;
-    margin: 0 0 0 8px;
-  }
+  ${({ firstEntry }) =>
+    firstEntry
+      ? `height: 40px`
+      : `  width: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+
+
+p {
+  font-family: 'Pretendard-Regular';
+  font-size: ${theme.fontSize.sm};
+  color: ${theme.colors.gray_500};
+  font-weight: 500;
+  line-height: 20px;
+  letter-spacing: 0em;
+  text-align: left;
+  margin: 0 0 0 8px;
+}`}
+`;
+
+export const SubmitTapeButton = styled(ButtonLayout)<ButtonProps>`
+  background-color: ${({ disabled }) =>
+    disabled ? theme.colors.disabled : theme.colors.primary};
 `;
