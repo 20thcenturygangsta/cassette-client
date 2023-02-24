4d65e9912b46a1b13911caa1ebec18e055ebdfc1
ddongule <mnk918p@gmail.com>
2023-02-24 23:44:52 +0900

    fix: 게스트 페이지 입장시 주황색 깜빡임 해결

--- src/pages/guest/[id]/guest-entry.tsx
+++ src/pages/guest/[id]/guest-entry.tsx
@@ -42,13 +42,13 @@ const GuestEntry = () => {
   const closeModal = () => setModalOpen(false);
   return (
     <>
-      {id ? (
+      {id && ownerName ? (
         <Container>
           <ModalPortal closeModal={closeModal}>
             {modalOpen && (
               <Modal
                 icon={<Cry />}
-                title={<>테이프를 남길 자리가 없어요!</>}
+                title="테이프를 남길 자리가 없어요!"
                 detail="친구들의 목소리가 담긴 테이프를 갖고싶나요?"
                 btnText="내 테이프 만들기"
                 link={MAKE_TAPE_URL}
